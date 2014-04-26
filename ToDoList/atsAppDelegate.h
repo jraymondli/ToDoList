be922b731ac0ea8ecdb832e97c7d202c1c6807b1
Raymond Li <rli@rli-mbp.local>
2014-04-26 11:40:39 -0700

    Initial Commit

---
+++ ToDoList/atsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  atsAppDelegate.h
+//  ToDoList
+//
+//  Created by Raymond Li on 4/26/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface atsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Raymond Li on 4/26/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "atsAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([atsAppDelegate class]));
+    }
+}
